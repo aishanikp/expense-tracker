6b89e2ab690bff057ae2ceaddafcc0ec77a19be4
Aishani K P <kpaishani@icloud.com>
2026-06-23 18:20:57 +0530

    Added total expenses in menu

--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -11,7 +11,8 @@ public class ExpenseTracker {
       System.out.println("-- Expense Tracker --");
       System.out.println("1. Add Expense");
       System.out.println("2. View Expenses");
-      System.out.println("3. Exit");
+      System.out.println("3. Show Total");
+      System.out.println("4. Exit");
       
       // Get user input and perform actions based on choice
       System.out.print("Enter your choice: ");
@@ -26,6 +27,9 @@ public class ExpenseTracker {
           viewExpenses();
           break;
         case 3:
+          total();
+          break;
+        case 4:
           // Exit logic
           System.out.println("Thank you for using the Expense Tracker!");
           System.exit(0);
@@ -39,6 +43,15 @@ public class ExpenseTracker {
     System.out.println("Enter expense amount: ");
     double amount = scanner.nextDouble();
     expenses.add(amount);
+    System.out.println("\n");
+    
+  }
+  static void total() {
+    double total = 0;
+    for (double expense : expenses) {
+      total += expense;
+    }
+    System.out.println("Total Expenses: " + total);
     System.out.println("\n");
     
   }
